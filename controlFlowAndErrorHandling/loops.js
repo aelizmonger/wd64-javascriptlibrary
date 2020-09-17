39489e837971ea495e10eee211d34faac2d7361d
Addy Monger <AddyMonger@Addys-MacBook-Air.local>
2020-09-17 15:48:12 -0400

    9/17

--- controlFlowAndErrorHandling/loops.js
+++ controlFlowAndErrorHandling/loops.js
@@ -120,4 +120,67 @@ Create a switch statement that takes in a value (number) that represents a grade
 *   F: 0-59
 */
 
-let grade = 92;
+//let grade = 92;
+
+for(var i = 0; i <= 9; i++) {
+    countToTen(i);
+}
+
+function countToTen() {
+    
+        if (i === 0) {
+            console.log(i + " the number is even")
+        }
+        else if (i % 2 ===0) {
+            console.log(i + " the number is even");
+        }
+        else {
+            console.log (i + " the number is odd");
+        }
+    }
+
+countToTen();
+
+for ( let j = 0;  j <  5; j++ ) {
+    console.log(j + " ")
+  }
+
+  for ( let j = 10;  j >  5; j-- ) {
+    console.log(j);
+  }
+
+  for ( let j = 5; j < 16; j++ ) {
+    console.log(j);
+  }
+
+  let count = 0;
+  for ( ;  count < 9; ++count ) {
+    console.log(count);
+  }
+
+  blah = function () {
+    return lala;
+    lala = "hi";
+  };
+  console.log(blah());
+
+  function sum(a, b) {
+    return a + b;
+  }
+  console.log(sum(1, 2, 3, 4));
+
+  function foo () {
+    return bar();
+    function bar() {
+      return "Poppin' bottles";
+    }
+  }
+  console.log(foo());
+
+  function whatDoesItDo(val) {
+    return val ? 1 : 2;
+  }; 
+
+  for ( let j = 0; j <= 10;j += 2) {
+    console.log(j);
+  }
